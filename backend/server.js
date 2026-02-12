f229373c0b51b822d1c129a129bbc672da1adac5
Yuukicoder <andbhe186315@fpt.edu.vn>
2026-02-12 17:22:39 +0700

    [yuukicoder/12-2] Create admin

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,8 @@
 import "./src/config/env.js"
-import app from "./src/app.js";
+import app from "./src/app.js"
 import db from "./src/config/db.js"
-import router from "./src/routes/index.js"
-console.log("acess 2 "+ process.env.JWT_ACCESS_SECRET);
-
 const PORT = process.env.PORT || 5000;
 db();
-app.use(router);
 app.listen(PORT, () => {
   console.log(` Server running on port ${PORT}`);
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,18 @@
-import express from "express";
-import cors from "cors";
+import express from "express"
+import cors from "cors"
+import morgan from "morgan"
 import router from "./routes/index.js"
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
-app.use(router);
+
 app.get("/", (req, res) =>{
     res.send("API running ...");
 });
+app.use(router);
 
 
 export default app;
