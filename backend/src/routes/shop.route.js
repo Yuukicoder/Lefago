3830c4fd024273063a87b430ed6945afee4aa753
Yuukicoder <andbhe186315@fpt.edu.vn>
2026-06-15 01:01:22 +0700

    [yuukicoder/15-6] API uploadMediaShop by Multer,  update API getMediaShop[filter, pagination]

--- backend/src/routes/shop.route.js
+++ backend/src/routes/shop.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addMediaShop, addShop, deleteMediaShop, deleteShop, getMediaShop, getShop, getShopByDestinationId, getShopBySlug, updateShop } from '../controllers/shops.controller.js';
+import { addMediaShop, addShop, deleteMediaShop, deleteShop, getMediaShop, getShop, getShopByDestinationId, getShopBySlug, updateShop, uploadShopMedia } from '../controllers/shops.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
 import { authorizeRoles } from '../middlewares/authorizeRoles.js';
 import { validate } from '../middlewares/validate.middleware.js';
 import { addShopSchema, updateShopSchema } from '../../validators/shop-validator.js';
 import { addMediaShopSchema } from '../../validators/shopMedia-validator.js';
+import { upload } from '../middlewares/upload.middleware.js';
 
 const router = express.Router();
 
@@ -17,6 +18,9 @@ router.delete('/:id', authMiddleware, authorizeRoles("shop"), deleteShop);
 
 // Media
 router.post('/:id/media', authMiddleware, authorizeRoles("shop"), validate(addMediaShopSchema), addMediaShop);
-router.get('/:id/media', getMediaShop);
+router.get('/media/:id', getMediaShop);
 router.delete('/media/:mediaId', authMiddleware, authorizeRoles("shop"),deleteMediaShop);
+
+// Multer
+router.post("/:id/media/upload", authMiddleware,authorizeRoles("shop"), upload.single("media"), uploadShopMedia);
 export default router;
--- backend/src/services/shopMedia.service.js
+++ backend/src/services/shopMedia.service.js
@@ -33,7 +33,7 @@ export const addMediaShop = async (shopId, data, userId) => {
   return result;
 };
 
-export const getMediaShop = async (shopId) => {
+export const getMediaShop = async (shopId, query) => {
   // logic here
   if(!mongoose.Types.ObjectId.isValid(shopId)){
     throw createHttpError(400, "Invalid Shop Id")
@@ -42,11 +42,44 @@ export const getMediaShop = async (shopId) => {
   if(!shop){
     throw createHttpError(404, "Do not have any shop")
   }
-  const result = await ShopMedia.find({shop_id: shopId}).populate("uploaded_by", "fullname email").lean();
-  if(!result || result.length === 0){
+  const {type, page = 1, limit = 10} = query;
+  const filter = {shop_id: shopId};
+  if(type){
+    filter.type = type;
+  }
+  const NumberPage = Number(page);
+  const NumberLimit = Number(limit);
+  const skip = (NumberPage-1)*NumberLimit;
+  const data = await ShopMedia.find(filter)
+                              .populate("shop_id", "name contact")
+                              .populate("uploaded_by", "fullname")
+                              .sort({createdAt: -1})
+                              .skip(skip)
+                              .limit(NumberLimit)
+                              .lean();
+  if(!data || data.length === 0){
     throw createHttpError(404, "Do not have any shop media")
   }
-  return result;
+  const formattedData = data.map(media => ({
+    _id: media._id,
+    type: media.type,
+    url: media.url,
+    shop: media.shop_id.name,
+    contact: media.shop_id.contact,
+    uploaded_by: media.uploaded_by.fullname
+
+  }))
+  const total = await ShopMedia.countDocuments(filter);
+  return {
+    formattedData, 
+    pagination:{
+      total,
+      page: NumberPage,
+      limit: NumberLimit,
+      totalPage: Math.ceil(total/NumberLimit)
+    }
+  }
+  
 };
 
 export const deleteMediaShop = async (mediaId, userId) => {
@@ -66,5 +99,24 @@ export const deleteMediaShop = async (mediaId, userId) => {
     throw createHttpError(403, "You cannot access")
   }
   const result = await ShopMedia.findByIdAndDelete(mediaId).lean();
+  return result;
+};
+
+
+export const uploadShopMedia = async (shopId, file, userId, req) => {
+  // logic here
+  if(!file){
+    throw createHttpError(400, "Invalid file")
+  }
+  const mediaType = file.mimetype.startsWith("video/")?"video":"image";
+  const mediaUrl = `${req.protocol}://${req.get("host")}/uploads/${file.filename}`;
+  const result = await addMediaShop(
+    shopId,
+    {
+      type: mediaType,
+      url: mediaUrl
+    },
+    userId
+  )
   return result;
 };
